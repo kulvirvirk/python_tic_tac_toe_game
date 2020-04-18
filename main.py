2066c0760c1e9a8fe24fa83ce0710dceb80c0239
kulvirvirk <kvirkdev@gmail.com>
2020-04-18 20:04:54 +0000

    created the board_values dict to hold possible values for the board

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
- 
+ # 1. create a dictionary to store the three rows(or 9 possible values) for the game these strings should be empty in the begining
+ # e.g. 
+
+ #    x| |O
+ #    ------
+ #    O|O|X
+ #    ------
+ #    x| |O
+  
+
+board_values = {
+  'top-L': '', 'top-M':'', 'top-R':'',
+  'mid-L': '', 'mid-M':'', 'mid-R':'',
+  'low-L': '', 'low-M':'', 'low-R':'', }
+
