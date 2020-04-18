b12c9eff2b1f67a9f0a0481dab036087c7b8dfb4
kulvirvirk <kvirkdev@gmail.com>
2020-04-18 23:30:52 +0000

    board printed using the dictionary values

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
  # 1. create a dictionary to store the three rows(or 9 possible values) for the game these strings should be empty in the begining
- # e.g. 
-
- #    x| |O
- #    ------
- #    O|O|X
- #    ------
- #    x| |O
-  
+ # 2. print out the board with default values from the board_values dictionary 
 
 board_values = {
-  'top-L': '', 'top-M':'', 'top-R':'',
-  'mid-L': '', 'mid-M':'', 'mid-R':'',
-  'low-L': '', 'low-M':'', 'low-R':'', }
+  'top-L': ' ', 'top-M':'X', 'top-R':' ',
+  'mid-L': 'X', 'mid-M':'X', 'mid-R':' ',
+  'low-L': 'O', 'low-M':'O', 'low-R':'O', }
 
+ # 2. print out the board with default values from the board_values dictionary 
+print(board_values['top-L'] + '|' + board_values['top-M'] + '|' + board_values['top-R'] + '\n'\
+ + '-----' + '\n'\
+ + board_values['mid-L'] + '|' + board_values['mid-M'] + '|' + board_values['mid-R']  + '\n'\
+ + '-----' + '\n'\
+ + board_values['low-L'] + '|' + board_values['low-M'] + '|' + board_values['low-R'] 
+  )
